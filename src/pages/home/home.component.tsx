86c965af8370e3226dbf1c9d818efadaca6e8db6
aliev008 <hfe.as@yandex.ru>
2023-09-01 01:39:29 +0300

    feat: added bar chart boxes and a styles for them

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,24 +1,46 @@
+import BarChartBox from "../../components/barChartBox/barChartBox.component";
 import ChartBox from "../../components/chartBox/chartBox.component";
 import TopBox from "../../components/topBox/topBox.component";
+
+import {
+  barChartBoxRevenue,
+  barChartBoxVisit,
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
+
 import "./home.style.scss";
 
 const Home = () => {
   return (
     <div className="home">
-    <div className="box box1">
-      <TopBox />
+      <div className="box box1">
+        <TopBox />
+      </div>
+      <div className="box box2">
+        <ChartBox {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartBox {...chartBoxProduct} />
+      </div>
+      <div className="box box4"></div>
+      <div className="box box5">
+        <ChartBox {...chartBoxConversion} />
+      </div>
+      <div className="box box6">
+        <ChartBox {...chartBoxRevenue} />
+      </div>
+      <div className="box box7">Box7</div>
+      <div className="box box8">
+        <BarChartBox {...barChartBoxVisit} />
+      </div>
+      <div className="box box9">
+        <BarChartBox {...barChartBoxRevenue} />
+      </div>
     </div>
-    <div className="box box2"><ChartBox /></div>
-    <div className="box box3"><ChartBox /></div>
-    <div className="box box4"><ChartBox /></div>
-    <div className="box box5"><ChartBox /></div>
-    <div className="box box6">Box6</div>
-    <div className="box box7">Box7</div>
-    <div className="box box8">Box8</div>
-    <div className="box box9">Box9</div>
-    </div>
-
-  )
-}
+  );
+};
 
 export default Home;
